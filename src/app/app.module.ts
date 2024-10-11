75f446504fdb977b55502d6caaafeeacaeacdc66
Dnyanesh K <dakanpurne@gmail.com>
2024-10-11 22:29:16 +0530

    called same counter in appcomponent too

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
 import { HomeComponent } from './home/home.component';
 import { counterReducer } from './states/counter/counter.reducer';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent],
   imports: [
+    CommonModule,
     BrowserModule,
     AppRoutingModule,
     StoreModule.forRoot({ counter: counterReducer }),
